53e22502825f90bdb0f82548aa01a4aab1de0595
Harlen Giraldo <harlen.giraldo@yuxiglobal.com>
2017-07-13 18:27:56 -0500

    publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   count= 0;
-  imgSource = './../favicon.ico';
+  imgSource = 'favicon.ico';
   avalible= false;
 }
